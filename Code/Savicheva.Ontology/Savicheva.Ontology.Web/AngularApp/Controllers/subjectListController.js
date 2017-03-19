99d960858dded0bc71ac706d41f089aadeb94daf
Mikhail Apsalikov <dee_jay@bk.ru>
2017-03-19 17:54:32 +0400

    Base service is implemented

---
+++ Code/Savicheva.Ontology/Savicheva.Ontology.Web/AngularApp/Controllers/subjectListController.js
@@ -0,0 +1,22 @@
+﻿(function () {
+	'use strict';
+
+	angular
+      .module('APP')
+      .controller('subjectListController', ['$scope', 'baseService', subjectListController]);
+
+	function subjectListController($scope, service) {
+		$scope.data = [];
+
+		$scope.reload = function() {
+			service.getList("subject").then(function(data) {
+				$scope.data = data;
+			});
+		};
+
+		function activate() {
+			$scope.reload();
+		}
+		activate();
+	}
+})();
---
+++ Code/Savicheva.Ontology/Savicheva.Ontology.Web/AngularApp/Services/baseService.js
@@ -0,0 +1,56 @@
+﻿(function () {
+	"use strict";
+	var angular = window.angular;
+	angular
+		.module("APP")
+		.service("baseService", ["$http", policyService]);
+
+	function policyService($http) {
+		return {
+			getList: function (controllerName, params) {
+				return $http({
+					url: "api/" + controllerName,
+					method: "GET",
+					params: params
+				});
+			},
+			getById: function (controllerName, id) {
+				return $http({
+					url: "api/" + controllerName,
+					method: "GET",
+					params: {
+						id: id
+					}
+				});
+			},
+			create: function (controllerName, entity) {
+				entity = angular.copy(entity);
+				return $http({
+					url: "api/" + controllerName,
+					method: "POST",
+					data: entity
+				});
+			},
+			update: function (controllerName, id, entity) {
+				entity = angular.copy(entity);
+				return $http({
+					url: "api/" + controllerName,
+					method: "PUT",
+					data: entity,
+					params: {
+						id: id
+					}
+				});
+			},
+			remove: function(controllerName, id) {
+				return $http({
+					url: "api/" + controllerName,
+					method: "DELETE",
+					params: {
+						id: id
+					}
+				});
+			}
+		};
+	}
+})();
