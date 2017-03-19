df03f1a1db049d5479bc4f6191f59642b946e20a
Mikhail Apsalikov <dee_jay@bk.ru>
2017-03-19 16:21:23 +0400

    Empty pages added

--- Code/Savicheva.Ontology/Savicheva.Ontology.Web/Controllers/HomeController.cs
+++ Code/Savicheva.Ontology/Savicheva.Ontology.Web/Controllers/HomeController.cs
@@ -1,22 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace Savicheva.Ontology.Web.Controllers
+﻿namespace Savicheva.Ontology.Web.Controllers
 {
-    public class HomeController : Controller
-    {
-        // GET: Home
-        public ActionResult Index()
-        {
-            return View();
-        }
+	using System.Web.Mvc;
 
-	    public ViewResult DisciplineList()
-	    {
-		    return View();
-	    }
-    }
+	public class HomeController : Controller
+	{
+		// GET: Home
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ViewResult DisciplineList()
+		{
+			return View();
+		}
+
+		public ViewResult DisciplineView()
+		{
+			return View();
+		}
+
+		public ViewResult DisciplineEdit()
+		{
+			return View();
+		}
+
+		public ViewResult Departments()
+		{
+			return View();
+		}
+
+		public ViewResult Department()
+		{
+			return View();
+		}
+
+		public ViewResult Login()
+		{
+			return View();
+		}
+
+		public ViewResult UserManagement()
+		{
+			return View();
+		}
+	}
 }
