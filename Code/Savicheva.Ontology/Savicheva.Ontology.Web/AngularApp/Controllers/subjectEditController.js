a71321c942bf4fd9ffd92723e59a4b826f801c01
Mikhail Apsalikov <dee_jay@bk.ru>
2017-05-02 21:25:18 +0400

    Multi-select replaced by checkboxes

--- Code/Savicheva.Ontology/Savicheva.Ontology.Web/AngularApp/Controllers/subjectEditController.js
+++ Code/Savicheva.Ontology/Savicheva.Ontology.Web/AngularApp/Controllers/subjectEditController.js
@@ -1,71 +1,80 @@
-﻿(function () {
-	'use strict';
+﻿(function() {
+	"use strict";
 	var angular = window.angular;
 	angular
-		.module('APP')
-		.controller('subjectEditController', ['$scope', 'baseService', 'loginService', 'messageService', subjectEditController]);
+		.module("APP")
+		.controller("subjectEditController",
+			["$scope", "baseService", "loginService", "messageService", subjectEditController]);
 
 	function subjectEditController($scope, service, loginService, messageService) {
+		$scope.focs = {};
 		$scope.subject = {};
 		$scope.userRole = loginService.roles[loginService.getUserInfo().Role];
-		$scope.isPageVisible = function () {
+		$scope.isPageVisible = function() {
 			return $scope.userRole !== loginService.roles[0];
 		};
-		$scope.formsOfControl = [
-			{
-				id: 25,
-				name: "Зачет"
-			},
-			{
-				id: 228,
-				name: "Экзамен"
-			}
-		];
 
-		$scope.getId = function () {
+		$scope.getId = function() {
 			if ($scope.id) {
 				return $scope.id;
 			}
 			var url = window.location.href.split("/");
 			return +url[url.length - 1];
-		}
-
-		$scope.reload = function () {
+		};
+		$scope.reload = function() {
 			if ($scope.getId()) {
-				service.getById("subject", $scope.getId()).then(function (data) {
+				service.getById("subject", $scope.getId()).then(function(data) {
 					if (data && data.FormsOfControl && data.FormsOfControl.length) {
-						for (var i = 0; i < data.FormsOfControl.length; i++) {
-							data.FormsOfControl[i] = data.FormsOfControl[i].Id + "";
-						}
+						$scope.focs.test = !!_.find(data.FormsOfControl,
+							function(item) {
+								return item.Id === 25;
+							});
+						$scope.focs.exam = !!_.find(data.FormsOfControl,
+							function(item) {
+								return item.Id === 228;
+							});
 					}
-					$('.select-picker-foc').selectpicker('val', data.FormsOfControl);
 
 					$scope.subject = data;
 				});
 			}
 		};
 
-		$scope.save = function () {
+		$scope.save = function() {
 			if (!$scope.subject.Title) {
 				messageService.error("Введите название дисциплины");
 				return;
 			}
 
+			if (!$scope.focs.test && !$scope.focs.exam) {
+				messageService.error("Дисциплина должна иметь хотя бы одну форму контроля");
+				return;
+			}
+
+			$scope.subject.FormsOfControl = [];
+			if ($scope.focs.test) {
+				$scope.subject.FormsOfControl.push(25);
+			}
+			if ($scope.focs.exam) {
+				$scope.subject.FormsOfControl.push(228);
+			}
+
 			if ($scope.getId()) {
-				service.update("subject", $scope.getId(), $scope.subject).then(function () {
+				service.update("subject", $scope.getId(), $scope.subject).then(function() {
 					messageService.success("Дисциплина успешно сохранена в онтологию");
 				});
 			} else {
-				service.create("subject", $scope.subject).then(function (data) {
+				service.create("subject", $scope.subject).then(function(data) {
 					$scope.id = data.ModifiedEntity.Id;
 					messageService.success("Дисциплина успешно создана и сохранена в онтологии");
 				});
 			}
-		}
+		};
 
 		function activate() {
 			$scope.reload();
 		}
+
 		activate();
 	}
 })();
